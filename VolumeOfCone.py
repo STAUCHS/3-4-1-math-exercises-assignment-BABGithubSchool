ab16a336c6c9a766dacde2a96226969d2df4a770
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 14:10:03 -0400

    volumeofcone

--- VolumeOfCone.py
+++ VolumeOfCone.py
@@ -1,7 +1,11 @@
 #-------------------------------------------------------------------------
 # Name:         Volume of a Cone
-# Purpose:		
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:		Computes the volume of a cone given the radius and height from the user.
+# Author:       Bassias, B
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+import math
+radius = input("Enter the radius (cm):")
+height = input("Enter the height (cm):")
+volume = round((math.pi*(int(radius)**2)*int(height)/3), 1)
+print("The volume of your cone is", volume, "cm^3.")
