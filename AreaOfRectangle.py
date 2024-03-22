b2b557af7aa297a1521b14bd5cd4a7a2aa3845e3
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 12:58:55 -0400

    aordone

--- AreaOfRectangle.py
+++ AreaOfRectangle.py
@@ -1,7 +1,10 @@
 #-------------------------------------------------------------------------
 # Name:         Area of Rectangle
-# Purpose:      
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:      Calculate the Area of a rectangle using the inserted side lengths
+# Author:       Bassias, B
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+length = float(input("Enter the length (cm):"))
+width = float(input("Enter the width (cm):"))
+area = (float(length)*float(width))
+print("The area of your rectangle is", area, "cm^2.")
