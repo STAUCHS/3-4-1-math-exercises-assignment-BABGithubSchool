fe006daa46e2203266d2b13092e91cf30743a8ec
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 13:31:27 -0400

    average doine

--- Average.py
+++ Average.py
@@ -1,7 +1,13 @@
 #-------------------------------------------------------------------------
 # Name:         Average
-# Purpose:		
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:		To calculate the grade average of a user for a single semester.
+# Author:       Bassias. Blake
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+course1 = float(input("What is your grade for course one?"))
+course2 = float(input("What is your grade for course two?"))
+course3 = float(input("What is your grade for course three?"))
+course4 = float(input("What is your grade for course four?"))
+average = ((course1+course2+course3+course4)/4)
+average = round(float(average))
+print("Your grade average is", average, "%.")
