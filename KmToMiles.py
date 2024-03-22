c524dee3ad002c9053ff2d1454b6f933d685dce7
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 13:56:57 -0400

    sync

--- KmToMiles.py
+++ KmToMiles.py
@@ -1,7 +1,9 @@
 #-------------------------------------------------------------------------
 # Name:         Km to Miles
-# Purpose:		
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:		Converts a distance in kilometers into miles.
+# Author:       Bassias, B
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+km = input("Enter the distance in km:")
+miles = round((int(km)*0.621371), 2)
+print(km, "km is", miles, "miles.")
