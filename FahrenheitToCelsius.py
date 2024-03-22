b0fa0b23dab0d8971d8c9a7b0a134019a7b056a2
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 13:50:12 -0400

    sync

--- FahrenheitToCelsius.py
+++ FahrenheitToCelsius.py
@@ -1,7 +1,9 @@
 #-------------------------------------------------------------------------
 # Name:         Fahrenheit to Celsius
-# Purpose:		
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:		Lets the user enter the temperature in Fahrenheit, and prints out the result in Celsius. 
+# Author:       Bassias, B
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+fahren = input("Enter the temperature in fahrenheit:")
+celc = ((int(fahren)-32)*5/9)
+print(fahren, "degrees fahrenheit is", int(celc), "degrees celcius.")
