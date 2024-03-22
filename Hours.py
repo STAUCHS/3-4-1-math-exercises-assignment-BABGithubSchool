fd3a21046eed4ee423428e5d1b794568579ec7ae
BABGithubSchool <blake.bassias26@ycdsbk12.ca>
2024-03-22 13:53:46 -0400

    sync

--- Hours.py
+++ Hours.py
@@ -1,7 +1,10 @@
 #-------------------------------------------------------------------------
 # Name:         Hours
-# Purpose:		
-# Author:       Last Name. First Initial
-# Created:      dd/mm/yyyy
+# Purpose:		Lets you enter a number of hours, and converts it to days and hours.
+# Author:       Bassias, B
+# Created:      22/03/2024
 #-------------------------------------------------------------------------
-
+hours = input("Enter the number of hours:")
+days = (int(hours)/24)
+newhours = (int(hours)%24)
+print(hours, "hours =", int(days), "days and", newhours, "hours.")
